f18faa4b778ab430fb9956a7646fffcb4799be72
Ahta-Shamul Hoque Emran <emranhaquee@gmail.com>
2026-03-25 04:28:03 +0600

    Enable running Django server on configurable port
    Added ability to run the Django server on a specified port.

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greatkart.settings')
+    port = os.getenv('PORT', 8000)  # Default to 8000 if PORT is not set
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

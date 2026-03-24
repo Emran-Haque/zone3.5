10d6014ce23823870e278fedacf0f80365f70d45
Ahta-Shamul Hoque Emran <emranhaquee@gmail.com>
2026-03-25 05:15:08 +0600

    logo and api

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -24,8 +24,11 @@ SECRET_KEY = '#k=o&lxlkbvwo-c4%v%d%8eur1h0(&-r!+hg)w@45z^da(v08a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['zone3dot5.onrender.com']
+CSRF_TRUSTED_ORIGINS = [
+    'https://zone3dot5.onrender.com',
+    'http://zone3dot5.onrender.com',
+]
 
 # Application definition
 
